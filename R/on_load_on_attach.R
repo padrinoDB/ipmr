cd6f4f96d20e00ae7cd675e4a958b09c1bab8efd
levisc8 <levisc8@gmail.com>
2022-11-29 18:54:16 -0500

    update start up so no longer warn about organization switch

--- R/on_load_on_attach.R
+++ R/on_load_on_attach.R
@@ -2,22 +2,10 @@
 
 .onAttach <- function(libname = find.package("ipmr"), pkgname = "ipmr") {
 
-  # Warn development users that package will soon be migrating to padrinoDB
-
-  desc <- utils::packageDescription("ipmr")
-
-  if(!is.null(desc$RemoteType)) {
-
-    if(isTRUE(desc$RemoteType == "github" && desc$GithubUsername == "levisc8"))
-
     packageStartupMessage(
-      "It looks like you've installed the development vesrion of ipmr from the",
-      " repository 'levisc8/ipmr'.\n",
-      "ipmr will soon be moving to the padrinoDB Github organization.\n",
-      "In the future, you will need to install the development",
-      " version from there.\nremotes::install_github('padrinoDB/ipmr')"
+      "Welcome to 'ipmr'! `browseVignettes('ipmr')` to get started."
     )
 
-  }
+
 
 }
